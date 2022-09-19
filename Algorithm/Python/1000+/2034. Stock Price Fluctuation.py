5e30781afae0d749a41115c5cbd3a1b39403aa5c
Asura <todaymofish@gmail.com>
2022-09-19 20:57:46 +0800

    Update

--- Algorithm/Python/1000+/2034. Stock Price Fluctuation.py
+++ Algorithm/Python/1000+/2034. Stock Price Fluctuation.py
@@ -33,5 +33,33 @@ class StockPrice:
         return min(self.prices)
 
 
-# Method 2: minHeap maxHeap
+# Method 2: sortedList
+from sortedcontainers import SortedList
 
+class StockPrice:
+
+    def __init__(self):
+        self.lookupDict = {}
+        self.currentT = 0
+        self.sortList = SortedList()
+
+        
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp > self.currentT:
+            self.currentT = timestamp
+        if timestamp in self.lookupDict:
+            self.sortList.remove(self.lookupDict[timestamp])
+        self.sortList.add(price)
+        self.lookupDict[timestamp] = price
+        
+            
+    def current(self) -> int:
+        return self.lookupDict[self.currentT]
+        
+        
+    def maximum(self) -> int:
+        return self.sortList[-1]
+    
+    
+    def minimum(self) -> int:
+        return self.sortList[0]
